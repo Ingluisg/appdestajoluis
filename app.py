7842c0e1fc461b35d312dd89a9c30d2bedaf98e1
Ingluisg <louisgarciar@gmail.com>
2025-09-17 11:50:50 -0600

    Update app.py
    add import os

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import streamlit as st
 import pandas as pd
 from io import BytesIO
 from datetime import datetime
+import os
 
 st.set_page_config(page_title="Destajo App — Núcleo (Móvil)", layout="wide")
 
